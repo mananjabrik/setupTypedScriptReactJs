1e587455446786fa001bcfe695c794080c61524e
mananjabrik <manan.jabrik@gmail.com>
2021-02-19 20:14:51 +0700

    install typed script

---
+++ src/component/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, explicabo
+        harum. Ipsum voluptas ab doloremque consequuntur fugit expedita
+        excepturi, soluta, numquam fuga quis alias suscipit at id perspiciatis
+        exercitationem veritatis!
+      </p>
+    </div>
+  );
+};
+
+export default Navbar;
