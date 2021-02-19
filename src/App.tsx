58f128940d215ede018d2a5aded2a89d25dbe264
mananjabrik <manan.jabrik@gmail.com>
2021-02-19 20:58:43 +0700

    fix sampah copy

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Container } from "@chakra-ui/react";
 import { Navbar } from "./component";
 
 function App() {
   return (
-    <div className="App">
+    <Container bgColor="teal">
       <Navbar content="test percobaan" />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    </Container>
   );
 }
 
